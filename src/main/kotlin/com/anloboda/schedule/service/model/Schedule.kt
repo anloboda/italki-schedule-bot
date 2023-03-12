5cb22b833e48050ea5869041e73fd0bd4dbc3637
Anna Loboda <ann.loboda@gmail.com>
2023-03-12 17:59:59 +0100

    fix template formatting

--- src/main/kotlin/com/anloboda/schedule/service/model/Schedule.kt
+++ src/main/kotlin/com/anloboda/schedule/service/model/Schedule.kt
@@ -7,16 +7,18 @@ import com.anloboda.schedule.toSortedZonedLessons
 const val GREEN_HEART_EMOJI = "\uD83D\uDC9A\uFE0F"
 const val EUROPE_KYIV_TIMEZONE = "Europe/Kiev"
 
-private const val MESSAGE_TEMPLATE = """
+var MESSAGE_TEMPLATE = """
     Today's lessons schedule $GREEN_HEART_EMOJI (Ukrainian time) 
     %s
-"""
+""".trimIndent()
+
 
 data class Schedule(
     val zonedLessons: List<ZonedLesson>,
 ) {
     companion object {
-        fun from(italkiLessons: List<ItalkiLesson>) = Schedule(italkiLessons.toSortedZonedLessons(zone = EUROPE_KYIV_TIMEZONE))
+        fun from(italkiLessons: List<ItalkiLesson>) =
+            Schedule(italkiLessons.toSortedZonedLessons(zone = EUROPE_KYIV_TIMEZONE))
     }
 
     fun toTelegramString() = MESSAGE_TEMPLATE.format(
