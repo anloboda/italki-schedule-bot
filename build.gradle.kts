2caa610fff31785b6813bb27c6e4ad24cf31ad8c
Anna Loboda <ann.loboda@gmail.com>
2023-03-18 15:23:32 +0100

    add tests logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.7.4"
 	id("io.spring.dependency-management") version "1.1.0"
+	id("org.junit.platform.gradle.plugin")
 	kotlin("jvm") version "1.7.22"
 	kotlin("plugin.spring") version "1.7.22"
+	id("org.junit.platform.gradle.plugin")
 }
 
 group = "com.anloboda"
@@ -55,4 +57,7 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+	testLogging {
+		events "passed", "skipped", "failed"
+	}
 }
