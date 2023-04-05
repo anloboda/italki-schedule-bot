a7e5b882394222fa6e5c7b5b685f0764a72d334d
Anna_Loboda <ann.loboda@gmail.com>
2023-04-05 14:50:19 +0300

    add logs and expose port

--- src/main/kotlin/com/anloboda/schedule/api/decoder/ItalkiApiErrorDecoder.kt
+++ src/main/kotlin/com/anloboda/schedule/api/decoder/ItalkiApiErrorDecoder.kt
@@ -3,11 +3,17 @@ package com.anloboda.schedule.api.decoder
 import com.anloboda.schedule.api.ItalkiApiErrorException
 import feign.Response
 import feign.codec.ErrorDecoder
+import org.slf4j.LoggerFactory
 import java.lang.Exception
 
 class ItalkiApiErrorDecoder : ErrorDecoder {
-    override fun decode(methodKey: String, response: Response?): Exception {
-        // TODO: add log here
+
+    private val logger = LoggerFactory.getLogger(ItalkiApiErrorDecoder::class.java)
+
+    override fun decode(methodKey: String, response: Response): Exception {
+        logger.error(
+            "Call to italki api failed with error, response_status: ${response.status()}, response_body: ${response.body()}"
+        )
         throw ItalkiApiErrorException()
     }
 }
